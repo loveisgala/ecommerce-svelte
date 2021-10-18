48bf304f5096d3511ee6ed2b8fe673c74a4f6612
loveisgala <gabriela.gk@hotmail.com>
2021-10-18 14:55:41 -0300

    Creates pages components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	import Login from './pages/Login.svelte';
+	import Checkout from './pages/Checkout.svelte';
+	import ProductTemplate from './pages/ProductTemplate.svelte';
+	import Products from './pages/Products.svelte';
+	import Home from './pages/Home.svelte';
+	import About from './pages/About.svelte';
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<h1>Hello</h1>
