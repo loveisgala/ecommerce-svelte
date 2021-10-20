aea158fcf160812b0e9720825023d9acca582169
loveisgala <gabriela.gk@hotmail.com>
2021-10-20 14:48:24 -0300

    create featured products component

---
+++ src/components/Products/Featured.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let title = '';
+  import products from '../../stores/defaultProducts';
+  import Product from './Product.svelte';
+  import Loading from '../Loading.svelte';
+
+  $: featured = $products.filter((p) => p.featured); 
+</script>
+
+
+{#if !featured.length}
+  <Loading/>
+{:else}
+  <section class="section">
+    <h2 class="section-title">{title}</h2>
+    <div class="products-center">
+      {#each featured as product (product.id)}
+        <Product {product}/>
+      {:else}
+        <Loading/>
+      {/each}
+    </div>
+  </section>
+{/if}
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,8 +1,11 @@
 <script>
   import Hero from "../components/Hero.svelte";
   import { Link } from "svelte-routing";
+  import Featured from "../components/Products/Featured.svelte";
 </script>
 
 <Hero>
   <Link to="/products" class="btn btn-primary btn-hero">shop now</Link>
 </Hero>
+
+<Featured title="featured products"/>
