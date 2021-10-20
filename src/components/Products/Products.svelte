9808871cd84b02cc995bb2164d3dc40eeb2b9055
loveisgala <gabriela.gk@hotmail.com>
2021-10-20 14:27:59 -0300

    create product single page

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,11 +1,15 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
+  export let title = '';
   import products from '../../stores/defaultProducts';
-
+  import Product from './Product.svelte';
 </script>
-<h1>
-  hello from products component
-</h1>
-{#each $products as product}
-  <h2>{product.title}</h2>
-{/each}
+
+
+<section>
+  <h2 class="section-title">{title}</h2>
+  <div class="products-center">
+    {#each $products as product (product.id)}
+      <Product {product}/>
+    {/each}
+  </div>
+</section>
