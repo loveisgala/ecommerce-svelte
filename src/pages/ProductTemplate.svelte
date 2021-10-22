52be1a77d1e9ce1423ccdb523e5789c280ebe74e
loveisgala <gabriela.gk@hotmail.com>
2021-10-22 15:15:02 -0300

    use svelte:head for single product

--- src/pages/ProductTemplate.svelte
+++ src/pages/ProductTemplate.svelte
@@ -9,6 +9,9 @@
   $: product = $products.find((item) => item.id === parseInt(id));
 </script>
 
+<svelte:head>
+  <title>{!product ? 'single product' : product.title}</title>
+</svelte:head>
 {#if !product}
   <Loading/>
 {:else}
