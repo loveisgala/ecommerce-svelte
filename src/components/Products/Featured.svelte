f9b47b5adb71ca442445438c696aa9bd058e1e97
loveisgala <gabriela.gk@hotmail.com>
2021-10-22 14:22:50 -0300

    adds derived store

--- src/components/Products/Featured.svelte
+++ src/components/Products/Featured.svelte
@@ -1,20 +1,19 @@
 <script>
   export let title = '';
-  import products from '../../stores/defaultProducts';
+  import products, {featuredStore} from '../../stores/defaultProducts';
   import Product from './Product.svelte';
   import Loading from '../Loading.svelte';
 
-  $: featured = $products.filter((p) => p.featured); 
 </script>
 
 
-{#if !featured.length}
+{#if !$featuredStore.length}
   <Loading/>
 {:else}
   <section class="section">
     <h2 class="section-title">{title}</h2>
     <div class="products-center">
-      {#each featured as product (product.id)}
+      {#each $featuredStore as product (product.id)}
         <Product {product}/>
       {:else}
         <Loading/>
--- src/stores/defaultProducts.js
+++ src/stores/defaultProducts.js
@@ -1,13 +1,19 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 import localProducts from '../localProducts';
 
 const store = writable(flattenProducts([...localProducts]));
 
+//flatten products
 function flattenProducts(data) {
   return data.map((item) => {
     let image = item.image.url;
     return {...item, image};
   })
 }
+
+//featured store
+export const featuredStore = derived(store, ($featured) => {
+  return $featured.filter((item) => item.featured);
+});
 
 export default store;
